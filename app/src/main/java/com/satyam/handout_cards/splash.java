586f05ca0c4f381d9b18358c61646cf335053e75
RaahatFoundation-123 <106668598+RaahatFoundation-123@users.noreply.github.com>
2022-11-02 19:49:30 +0530

    projects

--- app/src/main/java/com/satyam/handout_cards/splash.java
+++ app/src/main/java/com/satyam/handout_cards/splash.java
@@ -2,7 +2,10 @@ package com.satyam.handout_cards;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 public class splash extends AppCompatActivity {
 
@@ -10,6 +13,7 @@ public class splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        new Handler().postDelayed(() -> startActivity(new Intent(splash.this, MainActivity.class)), 3000);
     }
 }
